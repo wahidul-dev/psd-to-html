316fa4bd5aeb222db2eae5f5322e6e2fcd1acc15
Wahidul hasan Abir <wahidpreeenon@gmail.com>
2020-10-25 00:01:57 +0600

    rest html css sticky nav

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+
+	//sticky menu
+	$(".js--services-section").waypoint(function(direction){
+		if(direction=="down"){
+			$("nav").addClass("sticky");
+
+		}else{
+			$("nav").removeClass("sticky");
+		}
+	});
+
+	//mixitup (Portfolio Section)
 	var mixer=mixitup('.container');
 });
